06b870465acb15e51285c6581c08289b96cb4f2d
Drew Haines <drew@dmdevco.com>
2019-11-04 18:00:45 -0800

    remove require from passport config

--- config/passport.js
+++ config/passport.js
@@ -2,11 +2,10 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
-const keys = require("../config/keys");
 const opts = {};
 
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = process.env.SECRET || keys.secretOrKey;
+opts.secretOrKey = process.env.SECRET || require("../config/keys").secretOrKey;
 
 module.exports = passport => {
   passport.use(
